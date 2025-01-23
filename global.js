ef8064c11068704097dbde373f2007b9c6a05764
fanfanccluo <z9luo@ucsd.edu>
2025-01-23 13:15:44 -0800

    js update
    js update

--- global.js
+++ global.js
@@ -3,3 +3,37 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+  );
+
+currentLink.classList.add('current');
+
+if (currentLink) {
+    // or if (currentLink !== undefined)
+    currentLink.classList.add('current');
+  }
+
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    // add the rest of your pages here
+    {url: 'contact/', title:'Contact'},
+    {url: 'cv/',title: 'CV'},
+    {url: 'https://github.com/fanfanccluo', title: 'GitHub' }
+  ];
+
+
+let nav = document.createElement('nav');
+document.body.prepend(nav)
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+  
