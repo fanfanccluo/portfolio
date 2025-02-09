6103b9375da31c3492aee11ab93d74c388f2b129
fanfanccluo <z9luo@ucsd.edu>
2025-02-08 23:27:04 -0800

    fix
    fix project count

--- projects/projects.js
+++ projects/projects.js
@@ -57,6 +57,22 @@ const projects = await fetchJSON("../lib/projects.json");
 const projectsContainer = document.querySelector(".projects");
 renderProjects(projects, projectsContainer, "h2");
 
+
+async function loadProjects() {
+    const projects = await fetchJSON("../lib/projects.json");
+
+    const projectsContainer = document.querySelector(".projects");
+    renderProjects(projects, projectsContainer, "h2");
+
+    // Update the project count in the header
+    const projectCountElement = document.querySelector("#project-count");
+    if (projectCountElement) {
+        projectCountElement.textContent = projects.length; 
+    }
+}
+loadProjects();
+
+
 // Define color scale
 const colors = d3.scaleOrdinal(d3.schemeTableau10);
 
